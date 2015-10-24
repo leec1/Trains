a3db25b1f7696d16cf9c26de43a0b17e2955385d
Shay Cichocki <shay@helpscout.com>
2015-10-24 16:03:40 -0700

    Added project

---
+++ Assets/CardDragger.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+public var location:String;
+function Update() {
+}
+
+function updatePosition() {
+    Debug.Log('here');
+    if(this.location =="tableau") {
+        this.location = "discard";
+        transform.position = new Vector3(-1.06,-4.03,0);
+        this.GetComponent(SpriteRenderer).sortingLayerName="TopOfDiscard";
+    }
+}
---
+++ Assets/Gameobject.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+function Start () {
+    for(var i=0;i<4;i++) {
+        var crd = GameObject.Find("Card");
+        var cobj = Instantiate(crd, new Vector3(0,0,10), Quaternion.identity);
+        var cd:CardDragger = cobj.GetComponent("CardDragger");
+        cd.location = "tableau";
+    }
+}
+
+function Update () {
+    if (Input.GetMouseButtonDown(0)) {
+        moveCard();
+    }
+}
+function moveCard() {
+    var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    var hit = Physics2D.Raycast (ray.origin, ray.direction, Mathf.Infinity);
+    if(hit) {
+        var cd:CardDragger = hit.collider.gameObject.GetComponent("CardDragger");
+        cd.updatePosition();
+    }
+
+
+}
