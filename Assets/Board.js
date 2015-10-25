a19a7012a1bf546cded63925f944a72fac26c7bc
Shay Cichocki <shay@helpscout.com>
2015-10-24 17:12:22 -0700

    Added board.js

---
+++ Assets/Board.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+public class Board extends MonoBehaviour {
+	public var sprite:Sprite;
+	function Update() {
+	
+	}
+	function OnMouseUp() {
+		
+	}
+}
+public class Tile {
+	private var xCenter:int;
+	private var yCenter:int;
+	private var id:int;
+	public function Tile(xCenter, yCenter) {
+	
+	}
+
+}
