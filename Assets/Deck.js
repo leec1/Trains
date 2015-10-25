47794612756f789b14fec8b1e8acde4a7dd03583
Shay Cichocki <shay@helpscout.com>
2015-10-24 17:28:25 -0700

    Updated Deck.js

--- Assets/Deck.js
+++ Assets/Deck.js
@@ -1,17 +1,18 @@
 ﻿#pragma strict
-private var instantiated=false;
-function Update() {
+public class Deck extends MonoBehaviour {
+	private var instantiated=false;
+	function Update() {
 
-}
+	}
 
-function OnMouseDrag() {
-    var crd = GameObject.Find("Card");
-    if(!this.instantiated) {
-        this.instantiated=true;
-        Instantiate(crd,Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,10.0f)), Quaternion.identity);
-    }
-    
-}
-function OnMouseUp() {
+	function OnMouseDrag() {
+    	var crd = GameObject.Find("Card");
+    	if(!this.instantiated) {
+       	 this.instantiated=true;
+       	 Instantiate(crd,Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,10.0f)), Quaternion.identity);
+    	}
+	}
+	function OnMouseUp() {
 
+	}
 }
